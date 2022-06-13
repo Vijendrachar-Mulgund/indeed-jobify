c596dbffbadd24cd19699a023342a8252b4ac1a1
Vijendra-Achar <vijendra.1995@gmail.com>
2022-06-13 22:05:26 +0530

    Logo issues fixed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
-function App() {
+import IndeedLogo from "./assets/Logo-icons/Indeed_logo_full.svg";
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">Hello World</header>
+      <img src={IndeedLogo} alt="" />
     </div>
   );
-}
+};
 
 export default App;
