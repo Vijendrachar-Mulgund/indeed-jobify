10c37ae8f6e841f410b10672fad65d8d3789970a
Vijendrachar <vijendrachar.m@gmail.com>
2022-07-16 01:38:59 +0530

    Basic server setup is done ✔️

--- server/server.js
+++ server/server.js
@@ -1,1 +1,9 @@
-console.log("This is the server");
+import express, { response } from "express";
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`The server has been started on port http://loaclhost:${port}`);
+});
