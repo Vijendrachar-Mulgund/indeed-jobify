9bfeb08d52e29f28a8bf48fcc0b81bf4103c0e63
Vijendrachar <vijendrachar.m@gmail.com>
2022-07-16 13:59:06 +0530

    The environment variables configured

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
-import express, { response } from "express";
-
+// Initial Express import
+import express from "express";
+// Init the Experess server
 const app = express();
+
+// Imports
+import dotenv from "dotenv";
+
+// Configure the Environment variables
+dotenv.config();
 
 const port = process.env.PORT || 8000;
 
+// Server start
 app.listen(port, () => {
   console.log(`The server has been started on port http://loaclhost:${port}`);
 });
