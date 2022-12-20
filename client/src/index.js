995a362e1896334a53d988393737a37d66b90f99
Vijendra-Achar <vijendrachar.m@gmail.com>
2022-12-20 06:14:09 +0530

    The Ant Design library has been updated

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,10 @@ import { initBrowserFingerprint } from "./fingerprintjs";
 import { fetchAndActivateRemoteConfig } from "./firebase/remoteConfig";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import "normalize.css";
 import "./index.scss";
 import App from "./App";
-import "antd/dist/antd.min.css";
-import { store } from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
